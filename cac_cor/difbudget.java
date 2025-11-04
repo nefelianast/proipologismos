8de5ed6f73bb3a10ccd112c50f5c7bc71328d5af
christos0ypo <t8240155@aueb.gr>
2025-11-04 20:15:41 +0000

    kanw mia allagh

--- cac_cor/difbudget.java
+++ cac_cor/difbudget.java
@@ -1,7 +1,8 @@
 package cac_cor;
 public class difbudget {
-    private b
-    public difbuget( double a,double b) {
-
+    public String difpresbudget ( double budget1 ,double budget2 ) {
+    double dif = budget1 - budget2;
+    double pre = dif/budget2*100;
+    return dif>0?"αυξηθικε απο το πρωιγουμενο ετος "+pre+" %" : "μειοθηκε απο το πρωιγουμενο ετος "+(-pre)+" %";
     }
 }
