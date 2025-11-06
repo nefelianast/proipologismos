cd89054b43d23030eab39b823f98acb1e69bffdb
christos0ypo <t8240155@aueb.gr>
2025-11-06 17:52:55 +0000

    το ενα κομματι αφορα απλους υπολογισμους και το αλλο θα το χρησημοποιησεται οταν ενας χρηστης εισαγει δεδομενα

--- cac_cor/difbudget.java
+++ cac_cor/difbudget.java
@@ -1,8 +1,13 @@
 package cac_cor;
-public class difbudget {
+public class Difbudget {
     public String difpresbudget ( double budget1 ,double budget2 ) {
     double dif = budget1 - budget2;
     double pre = dif/budget2*100;
     return dif>0?"αυξηθικε απο το πρωιγουμενο ετος "+pre+" %" : "μειοθηκε απο το πρωιγουμενο ετος "+(-pre)+" %";
+    }
+    public String dif ( double budget1 ,double budget2 ) {
+    double dif = budget1 - budget2;
+    double pre = dif/budget2*100;
+    return dif>0?" me diafora"+pre+" %" : " me diafora "+(-pre)+" %";
     }
 }
--- cac_cor/negnum.java
+++
@@ -1,7 +0,0 @@
-package cac_cor;
-
-public  class negnum extends Exception {
-    public negnum(String mes) {
-        super(mes);
-    }
-}
