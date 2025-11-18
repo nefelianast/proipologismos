c16f8d13850ca5226aa5289614ff7bf83c7aa698
ChristosSot <t8240278@aueb.gr>
2025-11-18 18:10:06 +0200

    δημιουργια κλασης DataDownload

---
+++ src/main/java/DataConvert.java
@@ -0,0 +1,36 @@
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.json.JSONObject;
+import java.io.File;
+import java.io.FileWriter;
+
+public class DataConvert extends DataDownload{
+    public static void convertiontool() {
+        try {
+            File pdfFile = new File("proipologismos.pdf");
+             if (!pdfFile.exists()) {
+                System.out.println("Δεν βρέθηκε το αρχείο proipologismos.pdf στον τρέχοντα φάκελο");
+                return;
+            }
+            PDDocument document = PDDocument.load(pdfFile);
+
+            PDFTextStripper pdfStripper = new PDFTextStripper();
+            String text = pdfStripper.getText(document);
+            document.close();
+
+            JSONObject json = new JSONObject();
+            json.put("text", text);
+
+            FileWriter writer = new FileWriter("proipologismos.json");
+            writer.write(json.toString(4));
+            writer.close();
+            
+            
+            System.out.println("Ο Προιπολογισμός του ετους " +DataDownload.yearof + " μετατράπηκε σε JSON: proipologismos.json");
+            
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
