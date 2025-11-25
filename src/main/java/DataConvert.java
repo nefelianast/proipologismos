5317ffa29148b46d761304353606ef39bbb8fe48
ChristosSot <t8240278@aueb.gr>
2025-11-25 12:41:22 +0200

    Fix typo in output message for JSON conversion

--- src/main/java/DataConvert.java
+++ src/main/java/DataConvert.java
@@ -26,7 +26,7 @@ public class DataConvert extends DataDownload{
             writer.close();
             
             
-            System.out.println("Ο Προυπολογισμός του έτους " +DataDownload.yearof + " μετατράπηκε σε αρχείο JSON: proipologismos.json");
+            System.out.println("Ο προϋπολογισμός του έτους " +DataDownload.yearof + " μετατράπηκε σε αρχείο JSON: proipologismos.json");
             
 
         } catch (Exception e) {
