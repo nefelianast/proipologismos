6ced075df789c7255523022bd499cacf202c8e87
ioannispanayi <t8230234@aueb.gr>
2025-12-12 22:24:46 +0200

    addition of get

---
+++ Ministry.java
@@ -0,0 +1,16 @@
+package cac_cor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ministry {
+    private String name;
+    private List<Revenue> revenues;
+    private List<Expense> expenses;
+
+     public Ministry(String name) {
+        this.name = name;
+        this.revenues = new ArrayList<>();
+        this.expenses = new ArrayList<>();
+    }
+}
