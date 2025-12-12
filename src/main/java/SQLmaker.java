27850dc7a99c1cccc2ab756d0f1e5becd4da564d
ChristosSot <t8240278@aueb.gr>
2025-12-12 21:18:51 +0200

    δημιουργία κλάσης SQLmaker.java η οποία φτιάχνει tables για τα χρόνια 2023,2024,2025

---
+++ src/main/java/SQLmaker.java
@@ -0,0 +1,297 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class SQLmaker {
+     public void make() {
+        String DBurl = "jdbc:sqlite:src/main/resources/database/BudgetData.db";
+       
+
+        try {
+            Connection conn = DriverManager.getConnection(DBurl);
+            String sql1 = "CREATE TABLE IF NOT EXISTS revenue_2025 ("
+        +"total_revenue MONEY PRIMARY KEY,"
+        +"taxes MONEY,"
+        +"social_contributions MONEY,"
+        +"transfers MONEY,"
+        +"sales_of_goods_and_services MONEY,"
+        +"other_current_revenue MONEY,"
+        +"fixed_assets MONEY,"
+        +"debt_securities MONEY,"
+        +"loans MONEY,"
+        +"equity_securities_and_fund_shares MONEY,"
+        +"currency_and_deposit_liabilities MONEY,"
+        +"debt_securities_liabilities MONEY,"
+        +"loans_liabilities MONEY,"
+        +"financial_derivatives MONEY"
+        +");";
+        String sql2 = "CREATE TABLE IF NOT EXISTS expenses_2025 (" 
+        +"    total_expenses MONEY PRIMARY KEY," 
+        +"    employee_benefits MONEY," 
+        +"    social_benefits MONEY," 
+        +"    transfers MONEY," 
+        +"    purchases_of_goods_and_services MONEY," 
+        +"    subsidies MONEY," 
+        +"    interest MONEY," 
+        +"    other_expenditures MONEY," 
+        +"    appropriations MONEY," 
+        +"    fixed_assets MONEY,"  
+        +"    valuables MONEY," 
+        +"    loans MONEY,"  
+        +"    equity_securities_and_fund_shares MONEY," 
+        +"    debt_securities_liabilities MONEY," 
+        +"    loans_liabilities MONEY"  
+        +");";
+        String sql3 =
+        "CREATE TABLE IF NOT EXISTS ministries_2025 ("
+        +"total_ministries MONEY,"  
+        +"id INTEGER ,"
+        +"presidency_of_the_republic MONEY,"
+        +"hellenic_parliament MONEY,"
+        +"presidency_of_the_government MONEY,"
+        +"ministry_of_interior MONEY,"
+        +"ministry_of_foreign_affairs MONEY,"
+        +"ministry_of_national_defence MONEY,"
+        +"ministry_of_health MONEY,"
+    +"ministry_of_justice MONEY,"
+    +"ministry_of_education_religious_affairs_and_sports MONEY,"
+    +"ministry_of_culture MONEY,"
+    +"ministry_of_national_economy_and_finance MONEY,"
+    +"ministry_of_agricultural_development_and_food MONEY,"
+    +"ministry_of_environment_and_energy MONEY,"
+    +"ministry_of_labor_and_social_security MONEY,"
+    +"ministry_of_social_cohesion_and_family MONEY,"
+    +"ministry_of_development MONEY,"
+    +"ministry_of_infrastructure_and_transport MONEY,"
+    +"ministry_of_maritime_affairs_and_insular_policy MONEY,"
+    +"ministry_of_tourism MONEY,"
+    +"ministry_of_digital_governance MONEY,"
+    +"ministry_of_migration_and_asylum MONEY,"
+    +"ministry_of_citizen_protection MONEY,"
+    +"ministry_of_climate_crisis_and_civil_protection MONEY,"
+    +"PRIMARY KEY (id,total_ministries)"
+    +");";
+    String sql4 = "CREATE TABLE IF NOT EXISTS budget_summary_2025 ("
+    + "budget_result MONEY primary key,"
+    + "total_revenue MONEY,"
+    + "total_expenses MONEY,"
+    + "total_ministries MONEY,"
+    + "total_da MONEY,"
+    + "FOREIGN KEY (total_revenue) REFERENCES revenue_2025(total_revenue),"
+    + "FOREIGN KEY (total_expenses) REFERENCES expenses_2025(total_expenses),"
+    + "FOREIGN KEY (total_ministries) REFERENCES ministries_2025(total_ministries),"
+    + "FOREIGN KEY (total_da) REFERENCES decentralized_administrations_2025(total_da)"
+    + ");";
+
+    String sql5 = "CREATE TABLE IF NOT EXISTS decentralized_administrations_2025 ("
+    +"id INTEGER ,"
+    +"total_da MONEY,"
+    +"decentralized_administration_of_attica MONEY,"
+    +"decentralized_administration_of_thessaly_central_greece MONEY,"
+    +"decentralized_administration_of_epirus_western_macedonia MONEY,"
+    +"decentralized_administration_of_peloponnese_western_greece_and_ionian MONEY,"
+    +"decentralized_administration_of_aegean MONEY,"
+    +"decentralized_administration_of_crete MONEY,"
+    +"decentralized_administration_of_macedonia_thrace MONEY,"
+    +"PRIMARY KEY (id , total_da)"
+    + ");";
+    String sql6 = "CREATE TABLE IF NOT EXISTS revenue_2024 ("
+    +"total_revenue MONEY PRIMARY KEY,"
+    +"taxes MONEY,"
+    +"social_contributions MONEY,"
+    +"transfers MONEY,"
+    +"sales_of_goods_and_services MONEY,"
+    +"other_current_revenue MONEY,"
+    +"fixed_assets MONEY,"
+    +"debt_securities MONEY,"
+    +"loans MONEY,"
+    +"equity_securities_and_fund_shares MONEY,"
+    +"currency_and_deposit_liabilities MONEY,"
+    +"debt_securities_liabilities MONEY,"
+    +"loans_liabilities MONEY,"
+    +"financial_derivatives MONEY"
+    +");";
+    String sql7 = "CREATE TABLE IF NOT EXISTS expenses_2024(" 
+    +"    total_expenses MONEY PRIMARY KEY," 
+    +"    employee_benefits MONEY," 
+    +"    social_benefits MONEY," 
+    +"    transfers MONEY," 
+    +"    purchases_of_goods_and_services MONEY," 
+    +"    subsidies MONEY," 
+    +"    interest MONEY," 
+    +"    other_expenditures MONEY," 
+    +"    appropriations MONEY," 
+    +"    fixed_assets MONEY,"  
+    +"    valuables MONEY," 
+    +"    loans MONEY,"  
+    +"    equity_securities_and_fund_shares MONEY," 
+    +"    debt_securities_liabilities MONEY," 
+    +"    loans_liabilities MONEY"  
+    +     ");";
+        String sql8 =  "CREATE TABLE IF NOT EXISTS ministries_2024 ("
+    +"total_ministries MONEY,"  
+    +"id INTEGER ,"
+    +"presidency_of_the_republic MONEY,"
+    +"hellenic_parliament MONEY,"
+    +"presidency_of_the_government MONEY,"
+    +"ministry_of_interior MONEY,"
+    +"ministry_of_foreign_affairs MONEY,"
+    +"ministry_of_national_defence MONEY,"
+   +"ministry_of_health MONEY,"
+    +"ministry_of_justice MONEY,"
+    +"ministry_of_education_religious_affairs_and_sports MONEY,"
+    +"ministry_of_culture MONEY,"
+    +"ministry_of_national_economy_and_finance MONEY,"
+    +"ministry_of_agricultural_development_and_food MONEY,"
+    +"ministry_of_environment_and_energy MONEY,"
+    +"ministry_of_labor_and_social_security MONEY,"
+    +"ministry_of_social_cohesion_and_family MONEY,"
+    +"ministry_of_development MONEY,"
+    +"ministry_of_infrastructure_and_transport MONEY,"
+    +"ministry_of_maritime_affairs_and_insular_policy MONEY,"
+    +"ministry_of_tourism MONEY,"
+    +"ministry_of_digital_governance MONEY,"
+    +"ministry_of_migration_and_asylum MONEY,"
+    +"ministry_of_citizen_protection MONEY,"
+    +"ministry_of_climate_crisis_and_civil_protection MONEY,"
+    +"PRIMARY KEY (id,total_ministries)"
+    +");";
+        String sql9 = "CREATE TABLE IF NOT EXISTS budget_summary_2024 ("
+    + "budget_result MONEY primary key,"
+    + "total_revenue MONEY,"
+    + "total_expenses MONEY,"
+    + "total_ministries MONEY,"
+    + "total_da MONEY,"
+    + "FOREIGN KEY (total_revenue) REFERENCES revenue_2024(total_revenue),"
+    + "FOREIGN KEY (total_expenses) REFERENCES expenses_2024(total_expenses),"
+    + "FOREIGN KEY (total_ministries) REFERENCES ministries_2024(total_ministries),"
+    + "FOREIGN KEY (total_da) REFERENCES decentralized_administrations_2024(total_da)"
+    + ");";
+        String sql10 = "CREATE TABLE IF NOT EXISTS decentralized_administrations_2024 ("
+    +"id INTEGER ,"
+    +"total_da MONEY,"
+    +"decentralized_administration_of_attica MONEY,"
+    +"decentralized_administration_of_thessaly_central_greece MONEY,"
+    +"decentralized_administration_of_epirus_western_macedonia MONEY,"
+    +"decentralized_administration_of_peloponnese_western_greece_and_ionian MONEY,"
+    +"decentralized_administration_of_aegean MONEY,"
+    +"decentralized_administration_of_crete MONEY,"
+    +"decentralized_administration_of_macedonia_thrace MONEY,"
+    +"PRIMARY KEY (id , total_da)"
+    + ");";
+            String sql11 = "CREATE TABLE IF NOT EXISTS revenue_2023 ("
+        +"total_revenue MONEY PRIMARY KEY,"
+        +"taxes MONEY,"
+        +"social_contributions MONEY,"
+        +"transfers MONEY,"
+        +"sales_of_goods_and_services MONEY,"
+        +"other_current_revenue MONEY,"
+        +"fixed_assets MONEY,"
+        +"debt_securities MONEY,"
+        +"loans MONEY,"
+        +"equity_securities_and_fund_shares MONEY,"
+        +"currency_and_deposit_liabilities MONEY,"
+        +"debt_securities_liabilities MONEY,"
+        +"loans_liabilities MONEY,"
+        +"financial_derivatives MONEY"
+        +");";
+          String sql12 = "CREATE TABLE IF NOT EXISTS expenses_2023 (" 
+        +"    total_expenses MONEY PRIMARY KEY," 
+        +"    employee_benefits MONEY," 
+        +"    social_benefits MONEY," 
+        +"    transfers MONEY," 
+        +"    purchases_of_goods_and_services MONEY," 
+        +"    subsidies MONEY," 
+        +"    interest MONEY," 
+        +"    other_expenditures MONEY," 
+        +"    appropriations MONEY," 
+        +"    fixed_assets MONEY,"  
+        +"    valuables MONEY," 
+        +"    loans MONEY,"  
+        +"    equity_securities_and_fund_shares MONEY," 
+        +"    debt_securities_liabilities MONEY," 
+        +"    loans_liabilities MONEY"  
+        +");";  
+        String sql13 = " CREATE TABLE IF NOT EXISTS ministries_2023 ("
+        +"total_ministries MONEY,"  
+    +"id INTEGER ,"
+    +"presidency_of_the_republic MONEY,"
+    +"hellenic_parliament MONEY,"
+    +"presidency_of_the_government MONEY,"
+    +"ministry_of_interior MONEY,"
+    +"ministry_of_foreign_affairs MONEY,"
+    +"ministry_of_national_defence MONEY,"
+    +"ministry_of_health MONEY,"
+    +"ministry_of_justice MONEY,"
+    +"ministry_of_education_and_religious_affairs MONEY,"
+   + "ministry_of_culture_and_sports MONEY,"
+    +"ministry_of_finance MONEY,"
+    +"ministry_of_agricultural_development_and_food MONEY,"
+    +"ministry_of_environment_and_energy MONEY,"
+    +"ministry_of_labor_and_social_affairs MONEY,"
+    +"ministry_of_development_and_investments MONEY,"
+    +"ministry_of_infrastructure_and_transport MONEY,"
+   + "ministry_of_maritime_affairs_and_insular_policy MONEY,"
+    +"ministry_of_tourism MONEY,"
+    +"ministry_of_digital_governance MONEY,"
+    +"ministry_of_migration_and_asylum MONEY,"
+    +"ministry_of_citizen_protection MONEY,"
+    +"ministry_of_climate_crisis_and_civil_protection MONEY,"
+    +"PRIMARY KEY (id,total_ministries)"
+    +");";
+    String sql14 = "CREATE TABLE IF NOT EXISTS decentralized_administrations_2023 ("
+    +"id INTEGER ,"
+    +"total_da MONEY,"
+    +"decentralized_administration_of_attica MONEY,"
+    +"decentralized_administration_of_thessaly_central_greece MONEY,"
+    +"decentralized_administration_of_epirus_western_macedonia MONEY,"
+    +"decentralized_administration_of_peloponnese_western_greece_and_ionian MONEY,"
+    +"decentralized_administration_of_aegean MONEY,"
+    +"decentralized_administration_of_crete MONEY,"
+    +"decentralized_administration_of_macedonia_thrace MONEY,"
+    +"PRIMARY KEY (id , total_da)"
+    + ");";
+      String sql15 = "CREATE TABLE IF NOT EXISTS budget_summary_2023 ("
+    + "budget_result MONEY primary key,"
+    + "total_revenue MONEY,"
+    + "total_expenses MONEY,"
+    + "total_ministries MONEY,"
+    + "total_da MONEY,"
+    + "FOREIGN KEY (total_revenue) REFERENCES revenue_2023(total_revenue),"
+    + "FOREIGN KEY (total_expenses) REFERENCES expenses_2023(total_expenses),"
+    + "FOREIGN KEY (total_ministries) REFERENCES ministries_2023(total_ministries),"
+    + "FOREIGN KEY (total_da) REFERENCES decentralized_administrations_2023(total_da)"
+    + ");";
+        try (Statement stmt = conn.createStatement()) {
+            stmt.execute(sql1);
+            stmt.execute(sql2);
+            stmt.execute(sql3);
+            stmt.execute(sql4);
+            stmt.execute(sql5);
+            stmt.execute(sql6);
+            stmt.execute(sql7);
+            stmt.execute(sql8);
+            stmt.execute(sql9);
+            stmt.execute(sql10);
+            stmt.execute(sql11);
+            stmt.execute(sql12);
+            stmt.execute(sql13);
+            stmt.execute(sql14);
+            stmt.execute(sql15);
+            System.out.println("Table created successfully!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        
+        
+        
+
+    }
+    
+}
--- src/main/java/testmain.java
+++ src/main/java/testmain.java
@@ -2,8 +2,12 @@ import java.io.IOException;
 
 public class testmain{
     public static void main(String args[]) throws IOException{
-        System.out.println("αυτή η κλάση είναι test κλάση που θαχρησιμοποιει τις κλάσεις DataDownload, DataConvert, DataStorer");
-        DataDownload downloader = new DataDownload();
-        downloader.Download();
+        //System.out.println("αυτή η κλάση είναι test κλάση που θαχρησιμοποιει τις κλάσεις DataDownload, DataConvert, DataStorer");
+        //DataDownload downloader = new DataDownload();
+        //downloader.Download();
+        SQLmaker sql = new SQLmaker();
+        sql.make();
+
+
     }
 }
