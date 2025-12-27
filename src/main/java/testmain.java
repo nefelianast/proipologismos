df8720844d5e6c229b3451305c7d854877527015
ChristosSot <t8240278@aueb.gr>
2025-12-27 22:12:22 +0200

    κλάση για συγκρίσεις Comparisons.java

--- src/main/java/testmain.java
+++ src/main/java/testmain.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class testmain{
     public static void main(String args[]) throws IOException{
         System.out.println("αυτή η κλάση είναι test κλάση που θαχρησιμοποιει τις κλάσεις DataDownload, DataConvert, DataStorer, SQLmaker");
-        System.out.println("1:download 2:SQL");
+        System.out.println("1:download 2:SQL 3:Comnparisons");
         Scanner scan = new Scanner(System.in);
         int Y = scan.nextInt();
         if (Y==1) {
@@ -12,7 +12,10 @@ public class testmain{
         } else if (Y==2){
         SQLmaker sql = new SQLmaker();
         sql.make();
-        }
+        } else if (Y==3){
+        Comparisons com = new Comparisons();
+        com.comparisons_of_two_years(2025,2026);
+        } 
 
 
     }
